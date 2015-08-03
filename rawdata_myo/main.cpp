e4191f1e30bfd76905136184c38f002b8a98bcb5
Tiben Che <tibenche@TibendeMacBook-Pro.local>
2015-08-02 22:03:32 -0500

    Initial Commit

---
+++ rawdata_myo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rawdata_myo
+//
+//  Created by Tiben Che on 8/2/15.
+//  Copyright (c) 2015 Tiben Che. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
